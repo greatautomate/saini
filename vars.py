1694950cbf9662b7d8ef20f7b098f496cd837282
greatautomate <mail.palestinemail@gmail.com>
2025-12-17 17:28:21 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -7,17 +7,18 @@ API_ID = int(environ.get("API_ID", "1393027"))
 API_HASH = environ.get("API_HASH", "12c455c1c1ea2ababc4661d5b741f7ff")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "5480731852"))
+OWNER = int(environ.get("OWNER", "7527795504"))
 CREDIT = environ.get("CREDIT", "SATYAJIT")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '5680454765').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '7527795504').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7527795504').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
 
